1dc0cc98b6127d3f76daf10839b848e39f7c8620
priyankadanda <39923686+priyankadanda@users.noreply.github.com>
2019-09-27 15:12:36 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -248,13 +248,15 @@ $(document).on("click", "#btnAboutMe", function(){
 	//Set messages for 'about me' reply
 	var aboutMe1 = "Does this mean we're friends? 😁";
 	var id1 = "chat-"+(totalMessages - messagesRemaining).toString();
-	var aboutMe2 = "I'm 23 years old and I recently graduated from Purdue's grad school, where I studied Mechanical Engineering and Management";
+	var aboutMe2 = "I'm an Engineering Management grad from Purdue University, currently building an awesome SaaS platform as a Business Analyst";
 	var id2 = "chat-"+(totalMessages - messagesRemaining + 1).toString();
-	var aboutMe3 = "I love technology and I'm currently looking to join a product design team! If you've got any leads, please email me at pdanda@purdue.edu 👍";
+	var aboutMe3 = "Psst. I manage the project too! We can discuss this over some coffee ☕";
 	var id3 = "chat-"+(totalMessages - messagesRemaining + 2).toString();
+	var aboutMe4 = "I love technology and I'm currently looking to join a product team as a product manager! If you've got any leads, email me at danda_priyanka@outlook.com 👍";
+	var id4 = "chat-"+(totalMessages - messagesRemaining + 3).toString();
 	var botFollowUp = "Wanna see my work?";
 	var botGoodbye = "Thanks for hanging out! 👋"
-	var id4 = "chat-"+(totalMessages - messagesRemaining + 3).toString();
+	var id5 = "chat-"+(totalMessages - messagesRemaining + 4).toString();
 
 	//Only include bot follow up if this is not the final prompt
 	if ($(".replyButtons").children().length > 1) {
